bd52c77f3d2a6cac0cfb7f1cb31e6333538b3882
Agustín Méndez <matagus@gmail.com>
2024-09-08 09:11:07 -0500

    More tests, one for each possible column in tasks.csv

--- tests/cli.rs
+++ tests/cli.rs
@@ -30,6 +30,42 @@ fn run(args: &[&str], expected_file: &str) -> TestResult {
 fn test_default_column() -> TestResult {
     run(
         &["tests/inputs/tasks.csv"],
-        "tests/expected/default-column.text",
+        "tests/expected/default-column.txt",
+    )
+}
+
+// --------------------------------------------------
+#[test]
+fn test_first_column() -> TestResult {
+    run(
+        &["-c", "1", "tests/inputs/tasks.csv"],
+        "tests/expected/column-1.txt",
+    )
+}
+
+// --------------------------------------------------
+#[test]
+fn test_2nd_column() -> TestResult {
+    run(
+        &["-c", "2", "tests/inputs/tasks.csv"],
+        "tests/expected/column-2.txt",
+    )
+}
+
+// --------------------------------------------------
+#[test]
+fn test_3rd_column() -> TestResult {
+    run(
+        &["-c", "3", "tests/inputs/tasks.csv"],
+        "tests/expected/column-3.txt",
+    )
+}
+
+// --------------------------------------------------
+#[test]
+fn test_4th_column() -> TestResult {
+    run(
+        &["-c", "4", "tests/inputs/tasks.csv"],
+        "tests/expected/column-4.txt",
     )
 }
